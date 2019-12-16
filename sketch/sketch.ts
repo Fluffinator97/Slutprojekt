b0f43fbc0d729ddab30c805e6af7ee5cac8bccfe
Simon Beijer <beijersimon@gmail.com>
2019-12-16 12:33:36 +0100

    Start

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,5 +1,16 @@
-const gameMenu: GameMenu
-const 
+/* CONST */
+
+// const gameMenu: GameMenu;
+// const gameManager: GameManager;
+// const button: Button;
+// const sound: Sound;
+// const player: Player;
+// const world: World;
+// const gameControls: GameControls;
+// const alfredsPaddle: AlfredsPaddle;
+// const entity: Entity;
+// const dynamite: Dynamite;
+// const explosion: Explosion;
 
 /**
  * Built in preload function in P5
@@ -10,6 +21,12 @@ function preload() {
     // Tyvärr har jag inte fått till den globala typningen för
     // inladdningen av ljud men fungerar bra enligt nedan..
     // sound = (window as any).loadSound('../assets/mySound.wav');
+
+    
+
+
+
+
 }
 
 /**
@@ -19,11 +36,13 @@ function preload() {
  * in the draw function below
  */
 function setup() {
-    createCanvas(windowWidth, windowHeight)
-    frameRate(60)
-    noCursor()
-    fullscreen()
-    gameMenu = new GameMenu()
+    createCanvas(windowWidth / 2.5, windowHeight);
+    frameRate(60);
+    noCursor();
+    fullscreen();
+    // gameMenu = new GameMenu();
+
+    
 }
 
 /**
@@ -32,12 +51,17 @@ function setup() {
  * you created in the setup function above
  */
 function draw() {
-    background('blue')
-    fill('red')
-    stroke('white')
-    circle(width * .5, height * .5, width * 0.2)
-    gameMenu.draw()
+    background(100);
+    fill('red');
+    stroke('white');
+    circle(width * .5, height * .5, width * 0.07);
+    rect(width / 2, height - 20, width * .3, 15, 10);
+    arc(width / 2, height - 27, width * .2, height * .06, radians(180), radians(360));
+    rectMode(CENTER);
+    // gameMenu.draw();
+
 }
+
 
 
 /**
